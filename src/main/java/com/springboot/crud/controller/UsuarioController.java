1da0e0ab6dfcc95abb7523e630c99414066fe2af
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2022-11-20 23:47:45 -0300

    Adicionando ResponseStatus nos endpoints

--- src/main/java/com/springboot/crud/controller/UsuarioController.java
+++ src/main/java/com/springboot/crud/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -19,22 +20,26 @@ public class UsuarioController {
 	private UsuarioRepository usuarioRepo;
 
 	@GetMapping("/alunos")
+	@ResponseStatus(HttpStatus.OK)
 	public Page<Usuario> listar(Pageable pageable) {
 		return usuarioService.listar(pageable);
 	}
 
 	@GetMapping("/aluno/{id}")
+	@ResponseStatus(HttpStatus.OK)
 	public Optional<Usuario> buscar(@PathVariable Long id) {
 		return usuarioRepo.findById(id);
 	}
 
 	@PostMapping("/aluno")
+	@ResponseStatus(HttpStatus.CREATED)
 	public String adicionarUsuario(@RequestBody Usuario usuario) {
 		usuarioRepo.save(usuario);
 		return usuario.getNome() + " foi adicionado.";
 	}
 
 	@PutMapping("/aluno/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Usuario atualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
 		Usuario usuarioAtual = usuarioRepo.findById(id).get();
 		BeanUtils.copyProperties(usuario, usuarioAtual, "id");
